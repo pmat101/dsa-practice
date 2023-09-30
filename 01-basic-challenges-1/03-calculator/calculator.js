545ec80a0a7a0721158a32a11ccf950d7cbb88ee
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-09-30 20:33:28 +0530

    3rd problem in Basic-Challenges-01 done

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,8 @@
-function calculator() {}
+function calculator(a, b, c) {
+  if (c === "+") return a + b;
+  if (c === "-") return a - b;
+  if (c === "*") return a * b;
+  if (c === "/") return a / b;
+}
 
 module.exports = calculator;

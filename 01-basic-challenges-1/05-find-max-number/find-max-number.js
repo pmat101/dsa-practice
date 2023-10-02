df811dff14dd7855811fa208eca631c830a0a913
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-02 23:57:31 +0530

    01/05 work in progress

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,9 @@
-function findMaxNumber() {}
+function findMaxNumber(args) {
+  let max = args[0];
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > max) max = args[i];
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;

43ede10d662c20f88c438d6c22c6b18d8c6268d0
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-04 00:27:08 +0530

    problems 6, 7 of Basic Challenges 01 done

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(str) {
+  const arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (i == 0 || str[i - 1] == " ") arr.push(str[i].toUpperCase());
+    else arr.push(str[i]);
+  }
+  return arr.join("");
+}
 
 module.exports = titleCase;
--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,7 @@
-function reverseString() {}
+function reverseString(str) {
+  const arr = [];
+  for (let i = str.length - 1; i >= 0; i--) arr.push(str[i]);
+  return arr.join("");
+}
 
 module.exports = reverseString;

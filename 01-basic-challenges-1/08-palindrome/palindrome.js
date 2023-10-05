fd15014c5307785dfbffbd7551c59a0c59409139
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-05 14:41:02 +0530

    Problems 8, 9 of Basic Challenges I finished

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,10 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const splitArr = str.split(" ");
+  const splitStr = splitArr.join("");
+  const arr = [];
+  for (let i = splitStr.length - 1; i >= 0; i--) arr.push(splitStr[i]);
+  if (arr.join("") == splitStr) return true;
+  else return false;
+}
 
 module.exports = isPalindrome;
--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,17 @@
-function countVowels() {}
+function countVowels(str) {
+  let count = 0;
+  const newStr = str.toUpperCase();
+  for (let i = 0; i < newStr.length; i++) {
+    switch (newStr[i]) {
+      case "A":
+      case "E":
+      case "I":
+      case "O":
+      case "U":
+        count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;

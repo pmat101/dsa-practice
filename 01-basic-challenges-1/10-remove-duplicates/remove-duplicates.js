44a9b000ac5622e16431fc6997d38fb1d978bae7
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-06 12:03:51 +0530

    Q10 of Basic Challenges I, remove duplicates in an array

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,14 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  const arr2 = [];
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr2[j] === arr[i]) count = 1;
+    }
+    if (count == 0) arr2.push(arr[i]);
+    else count = 0;
+  }
+  return arr2;
+}
 
 module.exports = removeDuplicates;

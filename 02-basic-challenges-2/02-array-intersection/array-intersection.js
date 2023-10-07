75a9642b9b84455686e900b58f82e9badf49e066
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-07 18:48:20 +0530

    Q1 & Q2 of Basic Challenges II complete

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,11 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const arr3 = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr2[j] == arr1[i]) arr3.push(arr2[j]);
+    }
+  }
+  return arr3;
+}
 
 module.exports = arrayIntersection;

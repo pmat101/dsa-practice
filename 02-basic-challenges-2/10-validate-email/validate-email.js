47521495b0d66a9e16a4b193dcbca991e1c21606
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-12 14:01:59 +0530

    problem 10 of BASIC CHALLENGES II done

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,8 @@
-function validateEmail() {}
+function validateEmail(str) {
+    if(str.includes('@') && str.includes('.') && str[0] != '@' && str[0] != '.')
+        return true;
+    else
+        return false;
+}
 
 module.exports = validateEmail;

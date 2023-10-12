e68f744002211ec5688bf4a7e35c6c4cab8ef61b
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-12 13:42:41 +0530

    Problems 8 & 9 of BASIC CHALLENGES II complete

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,20 @@
-function diceGameSimulation() {}
+function diceGameSimulation(num) {
+    const arr = [];
+    for(i=0; i<num; i++){
+        let dice = () => {return Math.ceil(Math.random() * 6)};
+        let dice1 = dice();
+        let dice2 = dice();
+        let sum = dice1+dice2;
+        let result;
+        if(sum == 7 || sum == 11)
+            result = 'win';
+        else if(sum == 2 || sum == 3 || sum == 12)
+            result = 'lose';
+        else
+            result = 'roll again';
+        arr.push({ 'dice1': dice1, 'dice2': dice2, 'sum': sum, 'result': result });
+    }
+    return arr;
+}
 
 module.exports = diceGameSimulation;
--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,7 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+    return `(${arr[0]}${arr[1]}${arr[2]}) ${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}${arr[8]}${arr[9]}`;
+}
+
+console.log(formatPhoneNumber([0,9,8,4,8,4,9,0,4,4]))
 
 module.exports = formatPhoneNumber;

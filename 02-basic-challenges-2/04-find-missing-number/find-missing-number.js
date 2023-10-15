e2b05d1bae10fe62bcf4b74b070ec9f80a6d6b52
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-15 16:12:11 +0530

    bleh

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,8 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    for(let i=1; i<=arr.length; i++){
+        if(arr.includes(i) == false)
+        return i;
+    }
+}
 
 module.exports = findMissingNumber;

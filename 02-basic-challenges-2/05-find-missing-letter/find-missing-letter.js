63db8b4eefb1339a55c5a2b33d78b1a59c357b6a
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-17 14:54:48 +0530

    problems 5, 6 of Basic Challenges II done

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,10 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+    const start = arr[0].charCodeAt(0);
+    const end = arr[arr.length - 1].charCodeAt(0);
+    for(let i=start; i<=end; i++){
+        if(arr.includes(String.fromCharCode(i)) == false)
+        return String.fromCharCode(i);
+    }
+}
 
 module.exports = findMissingLetter;
--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,10 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[j] == str[i]) return false;
+    }
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;

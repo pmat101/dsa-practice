d2a075d18baf0d316ee00f9b908020983610b0c9
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-18 11:41:46 +0530

    Update first-non-repeating.js

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,16 @@
-function findFirstNonRepeatingCharacter() {}
+  
+function findFirstNonRepeatingCharacter(str){
+  for(let i=0; i<str.length; i++){
+    let count = 0;
+    for(let j=i+1; j<str.length; j++){
+      if(str[j] == str[i]){
+        count++;
+        break;
+      }
+    }
+    if(count == 0)
+      return str[i];
+  }
+}
 
 module.exports = findFirstNonRepeatingCharacter;

42ce8a5da3ca3a0b944a174dcca5730013f18283
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-19 12:03:34 +0530

    All basic challenges complete

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,18 +1,16 @@
-  
 function findFirstNonRepeatingCharacter(str){
   let count = 0;
   for(let i=0; i<str.length; i++){
-    for(let j=i+1; j<str.length; j++){
+    for(let j=0; j<str.length; j++){
       if(str[j] == str[i])
-        count=1;
+        count++;
     }
-    if(count == 0)
+    if(count <= 1)
       return str[i];
     else
       count = 0;
   }
-  if(count == 1)
-    return null;
+  return null;
 }
 
 module.exports = findFirstNonRepeatingCharacter;
